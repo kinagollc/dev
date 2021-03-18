ef8caeabbba80cf2117c03746e4caf3f987c9a4a
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-18 00:39:54 -0400

    Update search-list-2.php

--- protected/views/front/search-list-2.php
+++ protected/views/front/search-list-2.php
@@ -28,10 +28,11 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	     
 	       
 	       <h2><?php echo clearString($val['restaurant_name'])?></h2>
-	       <p class="merchant-address concat-text"><?php echo $val['merchant_address']?></p> 
 	       	       <p class="cuisine">
            <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
-           </p>       
+           </p>     
+	       <p class="merchant-address concat-text"><?php echo $val['merchant_address']?></p> 
+	         
 	       	       <div class="mytable">
 	         <div class="mycol">
 	            <div class="rating-stars" data-score="<?php echo $ratings['ratings']?>"></div>   
