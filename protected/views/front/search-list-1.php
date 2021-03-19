8a534642bc76a94d35b50555df5e081cbbe6a72c
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-19 02:30:47 -0400

    Update search-list-1.php

--- protected/views/front/search-list-1.php
+++ protected/views/front/search-list-1.php
@@ -40,6 +40,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
           <div class="mycol b">
           <div class="equal_table">
          <div class="col">
+         <a href="javascript:;" data-id="<?php echo $val['merchant_id']?>"  title="<?php echo t("add to your favorite restaurant")?>" class="add_favorites <?php echo "fav_".$val['merchant_id']?>"><i class="ion-android-favorite-outline"></i></a>
             <?php echo FunctionsV3::merchantOpenTag($merchant_id)?>
          </div>          
                   
@@ -48,7 +49,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
         </div> <!--mytable-->
 
         <div class="mytable">
-	        <a href="javascript:;" data-id="<?php echo $val['merchant_id']?>"  title="<?php echo t("add to your favorite restaurant")?>" class="add_favorites <?php echo "fav_".$val['merchant_id']?>"><i class="ion-android-favorite-outline"></i></a>
+	        
 	         <div class="mycol">
 	            	        <?php if($show_delivery_info):?>
 	        <p><?php echo t("<i class='fa fa-clock-o' aria-hidden='true'></i> ")?><?php echo !empty($val['delivery_estimation'])?$val['delivery_estimation']:t("not available")?></p>
