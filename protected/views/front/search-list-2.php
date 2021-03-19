96ea436e3a1ed3f7962815cd8058ec9afa809c22
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-18 23:59:25 -0400

    Update search-list-2.php

--- protected/views/front/search-list-2.php
+++ protected/views/front/search-list-2.php
@@ -17,7 +17,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
     <?php endif;?>
     
     <div class="row">
-    <p class="top15"><?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?></p>
+    <p><?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?></p>
 	    <div class="col-md-2 border ">
 	     <!--<a href="<?php echo Yii::app()->createUrl('store/menu/merchant/'.$val['restaurant_slug'])?>">-->
 	     <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>">
