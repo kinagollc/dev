fddae2e0c75379c09a84fd00c250d5b746f32f49
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-18 23:35:08 -0400

    Update search-list-2.php

--- protected/views/front/search-list-2.php
+++ protected/views/front/search-list-2.php
@@ -18,7 +18,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
     
     <div class="row">
      <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>">
-        <img src="<?php echo FunctionsV3::getMerchantHeader($merchant_id);?>" style="width:100%;height:100px;object-fit:cover">
+        <img src="<?php echo FunctionsV3::getMerchantHeader($merchant_id);?>" style="width:100%;height:100px;object-fit:cover;padding:20px;">
         </a>
 	    <div class="col-md-2 border ">
 	     <!--<a href="<?php echo Yii::app()->createUrl('store/menu/merchant/'.$val['restaurant_slug'])?>">-->
