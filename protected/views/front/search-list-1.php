2ce67b1c2773f8413eacb8bd364b6774513f9a70
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-19 01:39:37 -0400

    Update search-list-1.php

--- protected/views/front/search-list-1.php
+++ protected/views/front/search-list-1.php
@@ -38,7 +38,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
             <a href="javascript:;" data-id="<?php echo $val['merchant_id']?>"  title="<?php echo t("add to your favorite restaurant")?>" class="add_favorites <?php echo "fav_".$val['merchant_id']?>"><i class="ion-android-favorite-outline"></i></a>
          </div>          
         </div>
-             <div class="rating-stars" data-score="<?php echo $ratings['ratings']?>">  
+             <div class="rating-stars" data-score="<?php echo $ratings['ratings']?>"></div>   
              <?php if(is_array($ratings) && count($ratings)>=1):?>
              <?php endif;?>
           </div>
