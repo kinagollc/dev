38bb8801da5e44be73c3ec8dc549ebd0a11a0356
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-18 23:23:39 -0400

    dw

--- protected/views/front/search-list-1.php
+++ protected/views/front/search-list-1.php
@@ -24,7 +24,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
        
         
         <div class="mytable">
-          <div class="mycol a"><p class="concat-text shiftleft"><?php echo clearString($val['restaurant_name'])?></p>
+          <div class="mycol a"><p class="buzname concat-text shiftleft"><?php echo clearString($val['restaurant_name'])?></p>
         <p class="concat-text shiftleft"><?php echo $val['merchant_address']?></p>
          <p class="concat-text2 shiftleft">
         <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
