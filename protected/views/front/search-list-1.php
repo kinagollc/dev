c215910a984ab396780764e5a16dc4d496f46700
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-19 02:48:27 -0400

    Update search-list-1.php

--- protected/views/front/search-list-1.php
+++ protected/views/front/search-list-1.php
@@ -28,7 +28,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
           <div class="mycol a"><p class="buzname concat-text shiftleft"><?php echo clearString($val['restaurant_name'])?></p>
          <p class="concat-text3 shiftleft">
         <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
-        </p>          <div class="mycol">
+        </p>          <div class="mycol concat-text3">
 	         <?php 	        
 	        if(!$search_by_location){		        
 		        echo Yii::t("default","<i class='fa fa-location-arrow' aria-hidden='true'></i> [distance]",array(
