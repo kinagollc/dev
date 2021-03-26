1e5ebc68494a19ff1c5426dc9c3a4d0ff6132b5b
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-25 23:04:18 -0400

    Update search-list-1.php

--- protected/views/front/search-list-1.php
+++ protected/views/front/search-list-1.php
@@ -29,7 +29,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
          <p class="concat-text3 shiftleft">
         <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
         </p></div>
-
+        
           <div class="mycol b">
           <div class="equal_table">
           
@@ -41,8 +41,8 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
         </div>
          <!--mytable-->
 
-        <div class="mytable" style="margin-top:-10px;text-align:left!important;padding:5px;font-size:12px!important;">
-	        <div class="mycol" style="font-size:12px!important;">
+        <div class="mytable" style="margin-top:-20px;text-align:left!important;padding:5px">
+	        <div class="mycol">
 	        <?php 	        
 	        if(!$search_by_location){		        
 		        echo Yii::t("default","<i class='fa fa-location-arrow' aria-hidden='true'></i> [distance]",array(
@@ -50,12 +50,12 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 		        ));
 	        }
 	        ?></div>
-	         <div class="mycol" style="font-size:12px!important;">
+	         <div class="mycol">
 	            	        <?php if($show_delivery_info):?>
 	        <p><?php echo t("<i class='fa fa-clock-o' aria-hidden='true'></i> ")?><?php echo !empty($val['delivery_estimation'])?$val['delivery_estimation']:t("not available")?></p>
 	        <?php endif;?>
 	         </div>
-	         <div class="mycol" style="font-size:12px!important;"> 
+	         <div class="mycol"> 
 	            <p>
 	        <?php 	        
 	        if($show_delivery_info){
@@ -68,13 +68,13 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	         </div>
 	         
 	         <?php if($show_delivery_info):?>
-	         <div class="mycol" style="font-size:12px!important;">	          
+	         <div class="mycol">	          
 	          <p><?php echo t("<i class='fa fa-cart-plus' aria-hidden='true'></i> Min").": ".Price_Formatter::formatNumber($min_fees)?></p>
 	         </div>
 	         <?php endif;?>
 	         
 	         
-	         <div class="mycol" style="font-size:12px!important;">
+	         <div class="mycol">
 	            <?php if(method_exists('FunctionsV3','getOffersByMerchantNew')):?>
 	        <?php if ($offer=FunctionsV3::getOffersByMerchantNew($merchant_id)):?>
 	          <?php foreach ($offer as $offer_value):?>
@@ -83,9 +83,9 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	        <?php endif;?>
 	        <?php endif;?>
 	        
-	       
+	        <p class="top15"><?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?></p>
 	         </div>
-	          
+	         
 	       </div> 
         
         
