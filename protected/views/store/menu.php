76bca02dd7ff7ede1fc68e22369cba48700fb38f
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-03-30 17:37:36 -0400

    Update menu.php

--- protected/views/store/menu.php
+++ protected/views/store/menu.php
@@ -547,7 +547,161 @@ if ($food_viewing_private==2){
 	        
         </div>
         <!--END DELIVERY INFO-->
-                                             
+        <?php else :?>
+        
+        <?php endif;?>
+        
+        <!--CART-->
+        <div class="inner line-top relative">
+        
+           <i class="order-icon your-order-icon"></i>
+           
+           <p class="bold center"><?php echo t("Your Order")?></p>
+           
+           <div class="item-order-wrap"></div>
+           
+           <!--VOUCHER STARTS HERE-->
+           <?php //Widgets::applyVoucher($merchant_id);?>
+           <!--VOUCHER STARTS HERE-->
+           
+           <!--MAX AND MIN ORDR-->
+           <?php if ($minimum_order>0):?>
+           <div class="delivery-min">
+              <p class="small center"><?php echo Yii::t("default","Subtotal must exceed")?> 
+              <?php echo Price_Formatter::formatNumber($minimum_order);?>
+           </div>
+           <?php endif;?>
+           
+           <?php if ($merchant_minimum_order_pickup>0):?>
+           <div class="pickup-min">
+              <p class="small center"><?php echo Yii::t("default","Subtotal must exceed")?> 
+              <?php echo Price_Formatter::formatNumber($merchant_minimum_order_pickup);?>
+           </div>
+           <?php endif;?>
+                      
+           <?php if($minimum_order_dinein>0):?>
+           <div class="dinein-min">
+              <p class="small center"><?php echo Yii::t("default","Subtotal must exceed")?> 
+              <?php echo Price_Formatter::formatNumber($minimum_order_dinein)?>
+           </div>
+           <?php endif;?>
+              
+	        <a href="javascript:;" class="clear-cart">[<?php echo t("Clear Order")?>]</a>
+           
+        </div> <!--inner-->
+        <!--END CART-->
+                
+        <!--CONTACT LESS DELIVERY-->
+        <?php if($merchant_opt_contact_delivery==1):
+        $opt_contact_delivery = 0;
+        if(isset($_SESSION['kr_delivery_options']['opt_contact_delivery'])){
+        	$opt_contact_delivery = (integer)$_SESSION['kr_delivery_options']['opt_contact_delivery'];
+        }     
+        ?>
+        <div class="inner line-top relative center opt_contact_delivery_wrap">
+                    
+           <div class="row">
+             <div class="col-md-2">
+                <?php echo CHtml::checkBox('opt_contact_delivery',$opt_contact_delivery==1?true:false,array(
+                 'class'=>"icheck",                 
+                ))?>
+             </div>
+             <div class="col-md-10 text_left">
+              <p class="bold"><?php echo t("Opt in for no contact delivery")?></p>
+              <?php echo t("Our delivery executive will leave the order at your door/gate (not applicable for offline payment like COD)")?>
+             </div>
+           </div>
+          <!--box_green-->   
+        </div>
+        <?php endif;?>
+        <!--CONTACT LESS DELIVERY-->
+        
+        <!--DELIVERY OPTIONS-->
+        <div class="inner line-top relative delivery-option center">
+           <i class="order-icon delivery-option-icon"></i>
+           
+           <?php if ($remove_delivery_info==false):?>
+             <p class="bold"><?php echo t("Delivery Options")?></p>
+           <?php else :?>
+             <p class="bold"><?php echo t("Options")?></p>
+           <?php endif;?>
+           
+           <?php echo CHtml::dropDownList('delivery_type',$now,
+           (array)Yii::app()->functions->DeliveryOptions($merchant_id),array(
+             'class'=>'grey-fields'
+           ))?>
+                      
+           <?php             
+            if($website_use_date_picker==2){
+            	echo CHtml::dropDownList('delivery_date','',
+            	(array)FunctionsV3::getDateList($merchant_id)
+            	,array(
+            	  'class'=>'grey-fields date_list'
+            	));
+            } else {
+            	echo CHtml::hiddenField('delivery_date',$now);
+	            echo CHtml::textField('delivery_date1',
+	            FormatDateTime($now,false),array('class'=>"j_date grey-fields",'data-id'=>'delivery_date'));
+            }
+           ?>
+           
+           <div class="delivery_asap_wrap">            
+            <?php $detect = new Mobile_Detect;?>           
+            <?php if ( $detect->isMobile() ) :?>
+             <?php                           
+             echo CHtml::dropDownList('delivery_time',$now_time,
+             (array)FunctionsV3::timeList()
+             ,array(
+              'class'=>"grey-fields"
+             ))
+             ?>
+            <?php else :?>                       
+	          <?php
+	          $website_use_time_picker = getOptionA('website_use_time_picker');
+	          $delivery_time_list = FunctionsV3::getTimeList($merchant_id,$now);	          
+	          if($website_use_time_picker==3){
+	          	  echo CHtml::dropDownList('delivery_time','', (array)$delivery_time_list ,array(
+	          	    'class'=>'grey-fields time_list'
+	          	  ));
+	          } else {
+	          	  $now_time=date("h:i A");
+		          echo CHtml::textField('delivery_time',$now_time,
+		          array('class'=>"timepick grey-fields",'placeholder'=>Yii::t("default","Delivery Time")));
+	          }
+	          ?>
+	       <?php endif;?>  	          	  
+
+	          <?php if ( $checkout['is_pre_order']==2):?>         
+	          <span class="delivery-asap">
+	           <?php echo CHtml::checkBox('delivery_asap',false,array('class'=>"icheck"))?>
+	            <span class="text-muted"><?php echo Yii::t("default","Delivery ASAP?")?></span>	          
+	         </span>       	         	        	     
+	         <?php endif;?>    
+	         
+           </div><!-- delivery_asap_wrap-->
+           
+           
+           <?php if ( $checkout['code']==1):?>
+              <a href="javascript:;" class="orange-button medium checkout"><?php echo $checkout['button']?></a>
+           <?php else :?>              
+              <?php if ( $checkout['holiday']==1):?>
+                 <?php echo CHtml::hiddenField('is_holiday',$checkout['msg'],array('class'=>'is_holiday'));?>
+                 <p class="text-danger"><?php echo $checkout['msg']?></p>
+                 
+                 <?php if($checkout['is_pre_order']==1):?>
+                 
+                 <div style="margin:10px;">
+                 <a href="javascript:;" class="orange-button medium checkout"><?php echo $checkout['button']?></a>
+                 </div>
+                 
+                 <?php endif;?>
+              <?php else :?>
+                 <p class="text-danger"><?php echo $checkout['msg']?></p>
+                 <p class="small">
+                 <?php echo Yii::app()->functions->translateDate(date('F d l')."@".timeFormat(date('c'),true));?></p>
+              <?php endif;?>
+           <?php endif;?>
+                                                                
         </div> <!--inner-->
         <!--END DELIVERY OPTIONS-->
         
