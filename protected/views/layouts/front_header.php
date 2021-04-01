65d74c545c3e632f0614a3b6ef0048912fe35143
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-01 02:39:34 -0400

    Update front_header.php

--- protected/views/layouts/front_header.php
+++ protected/views/layouts/front_header.php
@@ -15,7 +15,7 @@
 <!--[if IE]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
 <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 <link rel="shortcut icon" href="<?php echo  Yii::app()->request->baseUrl; ?>/favicon.ico?ver=1.1" />
 <?php 
