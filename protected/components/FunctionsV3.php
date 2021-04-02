b6ae9110904972e1be0567b09596f900e99980f4
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-02 00:06:53 -0400

    Update FunctionsV3.php

--- protected/components/FunctionsV3.php
+++ protected/components/FunctionsV3.php
@@ -6995,7 +6995,7 @@ public static function sticPrettyTime($time='')
     		   }
     		} else return true;
     	}
-    	return false;
+    	return true;
     }
     
     public static function getReviewByOrder($client_id='', $order_id='')
