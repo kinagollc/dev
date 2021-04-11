57ae3f682cd980d2cb6961376f17439065590729
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-11 19:01:56 -0400

    Update menu-header.php

--- protected/views/front/menu-header.php
+++ protected/views/front/menu-header.php
@@ -37,7 +37,7 @@ data-image-src="<?php echo empty($background)?assetsURL()."/images/b-2.jpg":uplo
 	        <a href="javascript:;" data-id="<?php echo $merchant_id?>"  title="<?php echo t("add to your favorite places")?>" class="add_favorites <?php echo "fav_".$merchant_id?>"><i class="ion-android-favorite-outline"></i></a>
 	     </div>
 	     <div class="mycol">
-	     <img src="https://api.qrserver.com/v1/create-qr-code/?size=50x50&data=<?php echo clearString($restaurant_name)?>">
+	     <img src="https://api.qrserver.com/v1/create-qr-code/?size=50x50&data=<?php echo clearString($merchant_id)?>">
 	     </div>
 	   </div>
 	   
