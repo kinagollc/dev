a335543d5d5a90b24f21f18447c909c4d5a584b8
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-11 18:30:34 -0400

    Update menu-header.php

--- protected/views/front/menu-header.php
+++ protected/views/front/menu-header.php
@@ -37,7 +37,7 @@ data-image-src="<?php echo empty($background)?assetsURL()."/images/b-2.jpg":uplo
 	        <a href="javascript:;" data-id="<?php echo $merchant_id?>"  title="<?php echo t("add to your favorite places")?>" class="add_favorites <?php echo "fav_".$merchant_id?>"><i class="ion-android-favorite-outline"></i></a>
 	     </div>
 	     <div class="mycol">
-	     <a href="<?php Yii::app()->qrcode->create('http://www.wanzhoumo.com'); ?>">QR</a>
+	     qr
 	     </div>
 	   </div>
 	   
