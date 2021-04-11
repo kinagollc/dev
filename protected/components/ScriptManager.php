cb561db4727c0cc1ea71cceb85a50e4d78912c40
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-11 16:21:38 -0400

    qr6

--- protected/components/ScriptManager.php
+++ protected/components/ScriptManager.php
@@ -95,7 +95,7 @@ class ScriptManager
 		$cs->registerScriptFile($baseUrl."/assets/vendor/magnific-popup/jquery.magnific-popup.js"
 		,CClientScript::POS_END); 
 		
-		$cs->registerScriptFile($baseUrl."/assets/vendor/qrcode/qrcode.js"
+		$cs->registerScriptFile("https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js"
 		,CClientScript::POS_END); 
 				
 		
