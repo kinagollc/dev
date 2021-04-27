4f9dbedaa7340a1206ba1eb2db4ce7a738b85fa6
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-27 18:47:04 -0400

    s

--- protected/views/store/search-results-nodata.php
+++ protected/views/store/search-results-nodata.php
@@ -46,7 +46,7 @@ echo CHtml::hiddenField('current_page_url',isset($current_page_url)?$current_pag
 
 <div class="sections section-search-results">
   <div class="container center">
-     <h3><?php echo Yii::t("default","Looks like no businesses are signed up to Kinago around that area.")?></h3>
+     <h3><?php echo Yii::t("default","Oops. We're having trouble finding that address.")?></h3>
      <p><?php echo Yii::t("default","Please enter your address in one of the following formats and try again. Please do NOT enter your apartment or floor number here.")?></p>
     <p class="bold">- <?php echo Yii::t("default","Street address, city, state")?></p>
     <p class="bold">- <?php echo Yii::t("default","Street address, city")?></p>
