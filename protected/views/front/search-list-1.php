ce4a57be7e356e53146af1a49586d5402f3fcb53
kinagollc <65845017+kinagollc@users.noreply.github.com>
2021-04-27 18:48:29 -0400

    searc

--- protected/views/front/search-list-1.php
+++ protected/views/front/search-list-1.php
@@ -16,77 +16,97 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
         <?php endif;?>
 
         <!--<a href="<?php echo Yii::app()->createUrl('store/menu/merchant/'.$val['restaurant_slug'])?>" >-->
-         <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>">
-        <div style="background-image:url('<?php echo FunctionsV3::getMerchantHeader($merchant_id);?>');width:100%;height:140px;background-size:cover;background-position:center">
-        </div>
+        <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>">
+        <img class="logo-medium"src="<?php echo FunctionsV3::getMerchantLogo($merchant_id);?>">
         </a>
         
-        
-       
+        <h2 class="concat-text"><?php echo clearString($val['restaurant_name'])?></h2>
+        <p class="merchant-address concat-text"><?php echo $val['merchant_address']?></p>
         
         <div class="mytable">
-          <div class="mycol a"><p class="buzname concat-text shiftleft"><?php echo clearString($val['restaurant_name'])?></p>
-         <p class="concat-text3 shiftleft">
-        <i><?php echo FunctionsV3::displayCuisine($val['cuisine']);?></i>
-        </p></div>
+          <div class="mycol a">
+             <div class="rating-stars" data-score="<?php echo $ratings['ratings']?>"></div>   
+             <?php if(is_array($ratings) && count($ratings)>=1):?>
+             <p><?php echo $ratings['votes']." ".t("Reviews")?></p>
+             <?php endif;?>
+          </div>
+          <?php if($show_delivery_info):?>
+          <div class="mycol b">             
+             <?php echo Price_Formatter::formatNumber($min_fees)?>
+             <p><?php echo t("Minimum Order")?></p>
+          </div>
+          <?php endif;?>
+        </div> <!--mytable-->
+
+        <div class="top25"></div>
         
-          <div class="mycol b">
-          <div class="equal_table">
-          
+        <div class="equal_table">
          <div class="col">
-            <div align="right"><?php echo FunctionsV3::merchantOpenTag($merchant_id)?></div>
+            <?php echo FunctionsV3::merchantOpenTag($merchant_id)?>
+         </div>          
+         <div class="col">
+            <a href="javascript:;" data-id="<?php echo $val['merchant_id']?>"  title="<?php echo t("add to your favorite restaurant")?>" class="add_favorites <?php echo "fav_".$val['merchant_id']?>"><i class="ion-android-favorite-outline"></i></a>
          </div>          
         </div>
-          </div>
-        </div>
-         <!--mytable-->
-
-        <div class="mytable" style="margin-top:-10px;text-align:left!important;padding:5px">
-	        <div class="mycol">
-	        <?php 	        
-	        if(!$search_by_location){		        
-		        echo Yii::t("default","<div style='font-size:12px!important;'><i class='fa fa-location-arrow' aria-hidden='true'></i> [distance]</div>",array(
-		          '[distance]'=>$distance
-		        ));
-	        }
-	        ?></div> 
-	         <div class="mycol">
-	            	        <?php if($show_delivery_info):?>
-	        <p style="font-size:12px!important;"><?php echo t("<i class='fa fa-clock-o' aria-hidden='true'></i> ")?><?php echo !empty($val['delivery_estimation'])?$val['delivery_estimation']:t("not available")?></p>
-	        <?php endif;?>
-	         </div>
-	         <div class="mycol"> 
-	            <p style="font-size:12px!important;">
-	        <?php 	        
-	        if($show_delivery_info){
-		        if ($delivery_fee>0){
-		             echo t("<i class='fa fa-motorcycle' aria-hidden='true'></i> ")." ".Price_Formatter::formatNumber($delivery_fee);
-		        } else echo  t("<i class='fa fa-motorcycle' aria-hidden='true'></i> ")." ".t("Free Delivery");
-	        }
-	        ?>
-	        </p>               
-	         </div>
-	         
-	         <?php if($show_delivery_info):?>
-	         <div class="mycol">	          
-	          <p style="font-size:12px!important;"><?php echo t("<i class='fa fa-cart-plus' aria-hidden='true'></i> Min").": ".Price_Formatter::formatNumber($min_fees)?></p>
-	         </div>
-	         <?php endif;?>
-	         
-	         
-	         <div class="mycol">
-	            <?php if(method_exists('FunctionsV3','getOffersByMerchantNew')):?>
-	        <?php if ($offer=FunctionsV3::getOffersByMerchantNew($merchant_id)):?>
-	          <?php foreach ($offer as $offer_value):?>
-	            <p style="font-size:12px!important;"><?php echo $offer_value?></p>
-	          <?php endforeach;?>
-	        <?php endif;?>
-	        <?php endif;?>
-	        
-	        <p class="top15" style="display:none;"><?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?></p>
-	         </div>
-	         
-	       </div> 
+                
+        
+        <?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?>                        
+        
+        <p class="top15 cuisine concat-text">
+        <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
+        </p>                
+                                 
+        <p>
+        <?php 
+        if(!$search_by_location){
+	        echo Yii::t("default","Distance : [distance]",array(
+	          '[distance]'=>$distance
+	        ));
+        }
+        ?>
+        </p>
+        
+        <?php  if($show_delivery_info):?>
+        <p><?php echo t("Delivery Est")?>: <?php echo !empty($val['delivery_estimation'])?$val['delivery_estimation']:t("not available")?></p>
+        <?php endif;?>
+        
+        <p>
+        <?php         
+        if($show_delivery_info){
+	        if($distance_covered>0){
+	        	echo Yii::t("default","Delivery Distance : [distance]",array(
+	        	  '[distance]'=>MapsWrapper::prettyDistance($distance_covered,$unit_pretty)
+	        	));
+	        } else echo  t("Delivery Distance").": ".t("not available");
+        }
+        ?>
+        </p>
+                                
+        <p>
+        <?php 
+        //if($val['service']!=3){
+        if($show_delivery_info){
+	        if ($delivery_fee){
+	             echo t("Delivery Fee").": ".Price_Formatter::formatNumber($delivery_fee);
+	        } else echo  t("Delivery Fee").": ".t("Free Delivery");
+        }
+        ?>
+        </p>
+        
+        <?php if(method_exists('FunctionsV3','getOffersByMerchantNew')):?>
+        <?php if ($offer=FunctionsV3::getOffersByMerchantNew($merchant_id)):?>
+          <?php foreach ($offer as $offer_value):?>
+            <p><?php echo $offer_value?></p>
+          <?php endforeach;?>
+        <?php endif;?>
+        <?php endif;?>   
+        
+        <?php echo FunctionsV3::displayServicesList($val['service'])?>          
+            
+        <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>" 
+        class="orange-button rounded3 medium">
+        <?php echo t("Order Now")?>
+        </a>
         
         
     </div> <!--inner-->
--- protected/views/front/search-list-2.php
+++ protected/views/front/search-list-2.php
@@ -17,31 +17,18 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
     <?php endif;?>
     
     <div class="row">
-    <p><?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?></p>
 	    <div class="col-md-2 border ">
 	     <!--<a href="<?php echo Yii::app()->createUrl('store/menu/merchant/'.$val['restaurant_slug'])?>">-->
 	     <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>">
 	      <img class="logo-small"src="<?php echo FunctionsV3::getMerchantLogo($merchant_id);?>">
-	     </a>	          
+	     </a>	     
+	     <?php echo FunctionsV3::displayServicesList($val['service']);?>    
+	     <?php FunctionsV3::displayCashAvailable($merchant_id,true,$val['service'])?>      
 	    </div> <!--col-->
 	    
 	    <div class="col-md-7 border">
 	     
-	       
-	       <h2><?php echo clearString($val['restaurant_name'])?></h2>
-	       	       <p class="cuisine">
-           <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
-           </p>     
-	       <p class="merchant-address concat-text" style="margin-top:-10px"><?php echo $val['merchant_address']?>
-	        <?php 	        
-	        if(!$search_by_location){		        
-		        echo Yii::t("default","(<i class='fa fa-location-arrow' aria-hidden='true'></i> [distance])",array(
-		          '[distance]'=>$distance
-		        ));
-	        }
-	        ?></p> 
-	         
-	       	       <div class="mytable" style="margin-top:5px;">
+	       <div class="mytable">
 	         <div class="mycol">
 	            <div class="rating-stars" data-score="<?php echo $ratings['ratings']?>"></div>   
 	         </div>
@@ -53,31 +40,6 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	         <div class="mycol"> 
 	            <?php echo FunctionsV3::merchantOpenTag($merchant_id)?>                
 	         </div>
-	        
-	         
-	         <div class="mycol">
-	            <a href="javascript:;" data-id="<?php echo $val['merchant_id']?>"  title="<?php echo t("add to your favorite restaurant")?>" class="add_favorites <?php echo "fav_".$val['merchant_id']?>"><i class="ion-android-favorite-outline"></i></a>
-	         </div>
-	         
-	       </div> <!--mytable-->
-	       <div class="mytable">
-	        
-	         <div class="mycol">
-	            	        <?php if($show_delivery_info):?>
-	        <p><?php echo t("<i class='fa fa-clock-o' aria-hidden='true'></i> ")?><?php echo !empty($val['delivery_estimation'])?$val['delivery_estimation']:t("not available")?></p>
-	        <?php endif;?>
-	         </div>
-	         <div class="mycol"> 
-	            <p>
-	        <?php 	        
-	        if($show_delivery_info){
-		        if ($delivery_fee>0){
-		             echo t("<i class='fa fa-motorcycle' aria-hidden='true'></i> ")." ".Price_Formatter::formatNumber($delivery_fee);
-		        } else echo  t("<i class='fa fa-motorcycle' aria-hidden='true'></i> ")." ".t("Free Delivery");
-	        }
-	        ?>
-	        </p>               
-	         </div>
 	         
 	         <?php if($show_delivery_info):?>
 	         <div class="mycol">	          
@@ -86,7 +48,55 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	         <?php endif;?>
 	         
 	         <div class="mycol">
-	            <?php if(method_exists('FunctionsV3','getOffersByMerchantNew')):?>
+	            <a href="javascript:;" data-id="<?php echo $val['merchant_id']?>"  title="<?php echo t("add to your favorite restaurant")?>" class="add_favorites <?php echo "fav_".$val['merchant_id']?>"><i class="ion-android-favorite-outline"></i></a>
+	         </div>
+	         
+	       </div> <!--mytable-->
+	       
+	       <h2><?php echo clearString($val['restaurant_name'])?></h2>
+	       <p class="merchant-address concat-text"><?php echo $val['merchant_address']?></p>   
+	       	       	       
+	       <p class="cuisine">
+           <?php echo FunctionsV3::displayCuisine($val['cuisine']);?>
+           </p>                
+                                                       
+           <p>
+	        <?php 	        
+	        if(!$search_by_location){		        
+		        echo Yii::t("default","Distance : [distance]",array(
+		          '[distance]'=>$distance
+		        ));
+	        }
+	        ?>
+	        </p>
+	        	        
+	        <?php if($show_delivery_info):?>
+	        <p><?php echo t("Delivery Est")?>: <?php echo !empty($val['delivery_estimation'])?$val['delivery_estimation']:t("not available")?></p>
+	        <?php endif;?>
+	        
+	        <p>
+	        <?php 	        	        
+	        if($show_delivery_info){
+		        if($distance_covered>0){
+		        	echo Yii::t("default","Delivery Distance : [distance]",array(
+		        	  '[distance]'=>MapsWrapper::prettyDistance($distance_covered,$unit_pretty)
+		        	));
+		        } else echo  t("Delivery Distance").": ".t("not available");
+	        }
+	        ?>
+	        </p>
+	                                
+	        <p>
+	        <?php 	        
+	        if($show_delivery_info){
+		        if ($delivery_fee>0){
+		             echo t("Delivery Fee").": ".Price_Formatter::formatNumber($delivery_fee);
+		        } else echo  t("Delivery Fee").": ".t("Free Delivery");
+	        }
+	        ?>
+	        </p>
+	        
+	        <?php if(method_exists('FunctionsV3','getOffersByMerchantNew')):?>
 	        <?php if ($offer=FunctionsV3::getOffersByMerchantNew($merchant_id)):?>
 	          <?php foreach ($offer as $offer_value):?>
 	            <p><?php echo $offer_value?></p>
@@ -95,19 +105,6 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	        <?php endif;?>
 	        
 	        <p class="top15"><?php echo FunctionsV3::getFreeDeliveryTag($merchant_id)?></p>
-	         </div>
-	         
-	       </div>
-	       	       	                 
-                                                       
-           
-	        	        
-
-	        
-	                                
-	        
-	        
-	        
 	        
 	    
 	    </div> <!--col-->
@@ -117,7 +114,7 @@ if($val['service']==1 || $val['service']==2  || $val['service']==4  || $val['ser
 	      <!--<a href="<?php echo Yii::app()->createUrl('store/menu/merchant/'.$val['restaurant_slug'])?>" -->
 	      <a href="<?php echo Yii::app()->createUrl("/menu/". trim($val['restaurant_slug']))?>" 
          class="orange-button rounded3 medium">
-          <?php echo t("Browse")?>
+          <?php echo t("Order Now")?>
          </a>   
 	    
 	    </div>
